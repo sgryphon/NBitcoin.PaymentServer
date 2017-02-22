75b84caf457219b95616af60ab070ca34497d82f
Sly Gryphon <sgryphon@computer.org>
2017-02-23 09:03:42 +1000

    Add manual assemblyinfo file to web

---
+++ src/NBitcoin.PaymentServer.Web/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
